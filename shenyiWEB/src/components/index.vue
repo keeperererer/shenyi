f3b69dad672867a6e3fcb903a752663babee9293
keeperererer <2249269086@qq.com>
2020-03-30 00:28:12 +0800

    test

---
+++ shenyiWEB/src/components/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    heeeeeeeeeeee
+  </div>
+</template>
+<script>
+export default {
+  name: "index"
+};
+</script>
+<style lang="stylus" scoped>
+.container{
+  color:red;
+}
+</style>
